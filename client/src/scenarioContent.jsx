78105af640502c4d7c0481c4d6cbb34a296fcb7e
Thiara de Alwis <mq23793@bristol.ac.uk>
2026-03-19 00:01:14 +0000

    added scenarios

--- client/src/scenarioContent.jsx
+++ client/src/scenarioContent.jsx
@@ -1,14 +1,107 @@
 export const SCENARIO_TEXT = {
     A: {
-        shared: 'scenario A instructions',
-        role1: 'fact 1A',
-        role2: 'fact 2A',
-        role3: 'fact 3A'
+        shared: 'You are participating in a study, acting as an investigator in a murder case. Your goal is to figure out who killed Mr. Guion, a well-known car company owner.\n' +
+            'The Incident:\n' +
+            'Mr. Guion was found dead behind his house on a Saturday morning. It appears he was hit with a crowbar and then fell down the stairs. The autopsy shows he died from the fall sometime between 6:30 and 7:00 AM.\n' +
+            'You will work with other investigators (other participants) to decide who to prosecute. There are three suspects:\n' +
+            '- Eddie Sullivan: A handyman who worked for Guion.\n' +
+            '- Billy Prentice: The yardman at Guion\'s estate.\n' +
+            '- Mickey Malone: A car parts supplier and Guion\'s golfing buddy.\n' +
+            'Only one person committed the crime.You will discuss the case with other investigators and decide together who to prosecute.\n',
+        role1: 'In his initial interview, Billy Prentice said he did not see the Guions on Saturday. The detectives subsequently found out Billy lied and was at the Guions\' on Saturday morning.\n' +
+            'It had rained Friday evening, and Billy\'s tire tracks were found in the mud, so he must\n' +
+            'have driven to the Guions\' after Friday evening.\n' +
+            'Billy borrowed some money from Guion two days before.\n' +
+            'The crowbar belongs to Eddie, but Billy\'s fingerprints were partially on its handle.\n' +
+            'Guion informed Mickey of his decision to stop supporting Mickey\'s business the day before.\n' +
+            'Mickey said he left his house to pick Guion up for golf, but turned down and went to a café instead.\n' +
+            'Mickey knew Guion\'s house well, as he often picked Guion up for golf.\n' +
+            'Mickey and Guion had argued the previous week about a business dispute\n' +
+            'Eddie said he was dismantling a shed around Guion\'s house from 6:00 AM.\n' +
+            'Eddie says he and Billy usually work at the Guions\' residence on Saturday mornings.\n' +
+            'Billy’s car has a loud muffler, and it’s the only car in the neighborhood that makes such a racket.\n' +
+            'Eddie said he heard Billy\'s car leave Guion\'s house around 7:00 AM.\n' +
+            'Guion had recently fired Eddie\'s daughter after insulting her, which left Eddie furious, according to another employee.\n',
+        role2: 'In his initial interview, Billy Prentice said he did not see the Guions on Saturday. The detectives subsequently found out Billy lied and was at the Guions\' on Saturday morning.\n' +
+            'It had rained Friday evening, and Billy\'s tire tracks were found in the mud, so he must\n' +
+            'have driven to the Guions\' after Friday evening.\n' +
+            'Billy borrowed some money from Guion two days before.\n' +
+            'The crowbar belongs to Eddie, but Billy\'s fingerprints were partially on its handle.\n' +
+            'Guion informed Mickey of his decision to stop supporting Mickey\'s business the day before.\n' +
+            'Mickey said he left his house to pick Guion up for golf, but turned down and went to a café instead.\n' +
+            'Mickey knew Guion\'s house well, as he often picked Guion up for golf.\n' +
+            'Mickey and Guion had argued the previous week about a business dispute\n' +
+            'Eddie said he was dismantling a shed around Guion\'s house from 6:00 AM.\n' +
+            'Eddie says he and Billy usually work at the Guions\' residence on Saturday mornings.\n' +
+            'A parking authority officer was giving out parking tickets outside the coffee shop Mickey. claimed to visit at the time of Mr. Guion’s murder. The officer\'s records indicate Mickey. received a ticket at 6:37am.\n' +
+            'Eddie is hard of hearing and never wears his hearing aid while working at Guion\'s\n'+
+            'Billy said Eddie\'s tools were usually locked up, but the crowbar was oddly left out.\n',
+        role3: 'In his initial interview, Billy Prentice said he did not see the Guions on Saturday. The detectives subsequently found out Billy lied and was at the Guions\' on Saturday morning.\n' +
+            'It had rained Friday evening, and Billy\'s tire tracks were found in the mud, so he must\n' +
+            'have driven to the Guions\' after Friday evening.\n' +
+            'Billy borrowed some money from Guion two days before.\n' +
+            'The crowbar belongs to Eddie, but Billy\'s fingerprints were partially on its handle.\n' +
+            'Guion informed Mickey of his decision to stop supporting Mickey\'s business the day before.\n' +
+            'Mickey said he left his house to pick Guion up for golf, but turned down and went to a café instead.\n' +
+            'Mickey knew Guion\'s house well, as he often picked Guion up for golf.\n' +
+            'Mickey and Guion had argued the previous week about a business dispute\n' +
+            'Eddie said he was dismantling a shed around Guion\'s house from 6:00 AM.\n' +
+            'Eddie says he and Billy usually work at the Guions\' residence on Saturday mornings.\n' +
+            'Billy admitted touching the crowbar when he found it.\n' +
+            'The storeowner of Comic Book Nation found Mr. Guion’s wallet in a dumpster outside his store around 7:00am. He says he did not see it minutes before, and he heard a car speeding away as he found it. The car ran quietly.\n' +
+            'A waitress confirmed that Mickey came into the café around 6:30 that Saturday, drank 2 cups of coffee, and left. Mickey was at the café for 20 minutes.\n' +
+            'Nobody actually witnessed Eddie working that morning.\n'
     },
     B: {
-        shared: 'scenario B instructions',
-        role1: 'fact 1B',
-        role2: 'fact 2B',
-        role3: 'fact 3B'
+        shared: 'The International Sports Federation runs an international event similar to the Olympics every 4 years. Historically, the events have been well received and attracted millions of attendees from around the world. Thorough the years the ISF have worked with partners and sponsors from various industries to bring the event to various cities and cultures. \n' +
+            '\n' +
+            'As one of 3 decision makers at the ISF, you have been asked to sit on a committee to review the options and select the next host city based on reasonable criteria for a large, city-wide sporting event. \n' +
+            '\n' +
+            'For the event this year, 3 cities have submitted a bid to host: \n' +
+            'Eldoron\n' +
+            'Myloria\n' +
+            'Cragnio\n' +
+            '\n' +
+            'Together, you must choose the best city to host the event. \n',
+        role1: 'Residents of Cragnio have a reputation for being friendly and welcoming of visitors\n' +
+            'In the past, residents in Cragnio have complained about noise from large events\n' +
+            'Cragnio’s dry season coincides with the event dates, minimizing risk of rain\n' +
+            'There are lots of supportive local businesses in Myloria\n' +
+            'The shopping centers in Cragnio are popular with visitors\n' +
+            'Cragnio has comfortable evening temperatures\n' +
+            'Myloria hosts a community of volunteers eager to help with the event\n' +
+            'Eldoron is a popular destination for concerts and conferences\n' +
+            'Eldoron has been rated as a top tourist destination\n' +
+            'Myloria has a vibrant nightlife scene to keep visitors entertained\n' +
+            'Eldoron has a reputation for an easy-to-navigate public transportation experience\n' +
+            'The Low sun index in Myloria minimizes risk of sunburn\n' +
+            'The variety of international cuisine in Cragnio may be limited\n',
+        role2: 'Visitors to Myloria occasionally complain about traffic in the city\n' +
+            'Residents of Cragnio have a reputation for being friendly and welcoming of visitors\n' +
+            'Eldoron has been rated as a top tourist destination\n' +
+            'Cragnio has comfortable evening temperatures\n' +
+            'Myloria hosts a community of volunteers eager to help with the event\n' +
+            'Myloria has a vibrant nightlife scene to keep visitors entertained\n' +
+            'The Low sun index in Myloria minimizes risk of sunburn\n' +
+            'Eldoron is a popular destination for concerts and conferences\n' +
+            'Cragnio’s dry season coincides with the event dates, minimizing risk of rain\n' +
+            'The shopping centers in Cragnio are popular with visitors\n' +
+            'Evenings in Myloria can be warmer than average, causing slight discomfort\n' +
+            'There are lots of supportive local businesses in Myloria\n' +
+            'Eldoron has dedicated cultural villages for visitors during major events\n',
+        role3: 'Residents of Cragnio have a reputation for being friendly and welcoming of visitors\n' +
+            'Eldoron has been rated as a top tourist destination\n' +
+            'The shopping centers in Cragnio are popular with visitors\n' +
+            'Eldoron has a strong culture of sports and fitness\n' +
+            'Cragnio has comfortable evening temperatures\n' +
+            'There are lots of supportive local businesses in Myloria\n' +
+            'Myloria hosts a community of volunteers eager to help with the event\n' +
+            'Myloria has a vibrant nightlife scene to keep visitors entertained\n' +
+            'Cragnio’s dry season coincides with the event dates, minimizing risk of rain\n' +
+            'The Low sun index in Myloria minimizes risk of sunburn\n' +
+            'Eldoron is a popular destination for concerts and conferences\n' +
+            'Some visitors to Cragnio with cars complain about parking availability\n' +
+            'Many people say Eldoron has friendly and welcoming residents\n' +
+            '\n'
     }
 };
